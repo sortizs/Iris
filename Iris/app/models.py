bf1dc45237eb8628bb08dd4f2561367711c8e582
sortizs <sortizs@eafit.edu.co>
2018-11-04 18:26:32 -0500

    participant model

--- Iris/app/models.py
+++ Iris/app/models.py
@@ -4,4 +4,9 @@ Definition of models.
 
 from django.db import models
 
-# Create your models here.
+class Participant(models.Model):
+    name = models.CharField(max_lenght=254)
+    lastname = models.CharField(max_lenght=254)
+    email = models.EmailField(max_lenght=254)
+    phone = models.CharField(max_lenght=254)
+    age = models.IntegerField(max_lenght=2)
